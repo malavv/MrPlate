99d33979c39f4cbbaadb46c1a9263ed54f984c19
Maxime Lavigne (malavv) <duguigne@gmail.com>
2017-07-27 01:02:52 -0400

    Added the base of the app on RTOS.
    The basic initialization and initial menu is present.
    
      - For some reason the Display Library needs to be display twice.
      - You might have to modify the Adafruit_GFX_config file once
      downloaded to add the Abstract Functions.

--- code-mbed/src/main.cpp
+++ code-mbed/src/main.cpp
@@ -1,34 +1,76 @@
 #include "mbed.h"
 #include "rtos.h"
+#include "USBSerial.h"
+#include "Adafruit_SSD1306.h"
 
+#include <string.h>
+#include <string>
+
+// Global Objects
 Queue<uint32_t, 5> queue;
+DigitalOut debugLed(LED1);
+USBSerial com;
+const bool kIsDebug = true;
+const char* kWelcomeMsg = "MrPlate welcomes you to a yeast extravaganza v1.0\n";
+std::string logs;
+struct I2c : I2C { I2c(PinName sda, PinName scl) : I2C(sda, scl)  { frequency(400000); start(); }; } i2c(D18, D19);
+Adafruit_SSD1306_I2c display(i2c, D20);
+Thread thread_cmd;
 
-DigitalOut myled(LED1);
+// Pre Declaration of Functions
+void com_menu();
 
-void queue_isr() {
-    queue.put((uint32_t*)2);
-    myled = !myled;
-}
-
-void queue_thread(void const *args) {
-    while (true) {
-        queue.put((uint32_t*)1);
-        Thread::wait(1000);
-    }
-}
-
+// Entry point
 int main (void) {
-    Thread thread(queue_thread);
+  logs.append("[INFO] MrPlate is initializing\n");
+  thread_cmd.start(com_menu);
+  debugLed = kIsDebug;
 
-    Ticker ticker;
-    ticker.attach(queue_isr, 1.0);
+  // Initial Text
+  logs.append("[INFO] Printing Welcome Text\n");
+  display.setTextWrap(true);
+  display.setTextColor(WHITE);
+  display.setTextCursor(0, 0);
+  display.clearDisplay();
+  display.printf(kWelcomeMsg);
+  display.display();
+  display.display();
 
-    while (true) {
-        osEvent evt = queue.get();
-        if (evt.status != osEventMessage) {
-            printf("queue->get() returned %02x status\n\r", evt.status);
-        } else {
-            printf("queue->get() returned %d\n\r", evt.value.v);
-        }
-    }
+  Thread::wait(2000);
+
+  // Print Menu
+  logs.append("[INFO] Printing Menu\n");
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextCursor(10, 0);
+  display.printf("-- Choose mode --\n");
+  display.setTextCursor(0, 32 / 2);
+  display.printf("Simple\n");
+  display.setTextCursor(128 / 2, 32 / 2);
+  display.printf("Advanced\n");
+  display.display();
+  display.display();
+
+  // Print Choice
+  logs.append("[INFO] Printing Default Simple Choice\n");
+  const char choice = 'L';
+  display.drawFastHLine(0, 32 - 2, (128 / 2) - 10,
+                        choice == 'L'? WHITE : BLACK);
+  display.drawFastHLine(128 / 2, 32 - 2, (128 / 2) - 10,
+                        choice == 'R' ? WHITE : BLACK);
+  display.display();
+  display.display();
+
+  while (true) { /* nop */ }
+}
+
+void com_menu() {
+  char str[80];
+  while(true) {
+    com.scanf("%79s", str);
+    if (strcmp(str, "logs") == 0)
+      com.printf(logs.c_str());
+    else
+      com.printf("Unrecognized command %s\n", str);
+  }
 }
